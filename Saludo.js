defe95c5ef30943f9b1348bc12339fab955fa8fa
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-12 12:35:39 +0100

    Create Saludo.js

---
+++ Saludo.js
@@ -0,0 +1,29 @@
+<script language="JavaScript" type="text/javascript">
+//<![CDATA[
+<!--
+today = new Date()
+if(today.getMinutes() < 10){
+pad = "0"}
+else
+pad = "";
+document.write ;if((today.getHours() >=6) && (today.getHours() <=9)){
+document.write("BUENOS DIAS!!")
+}
+if((today.getHours() >=10) && (today.getHours() <=11)){
+document.write("COFFEE TIME!!")
+}
+if((today.getHours() >=12) && (today.getHours() <=17)){
+document.write("QUE APROVECHE..")
+}
+if((today.getHours() >=18) && (today.getHours() <=23)){
+document.write("BUENA TARDE..")
+}
+if((today.getHours() >=0) && (today.getHours() <=3)){
+document.write("..BUENAS NOCHES!! ")
+}
+if((today.getHours() >=4) && (today.getHours() <=5)){
+document.write("DUERME BIEN..")
+}
+// -->
+//]]>
+</script>
